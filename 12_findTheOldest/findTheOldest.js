00d8d7ff908c92859fd8a28011a6facca7027a4c
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-04-23 16:51:19 -0400

    Finalized 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    people.map(person => {
+        if (isNaN(person.yearOfDeath)) {
+            person.yearOfDeath = new Date().getFullYear()
+        }
+    })
 
+    return people
+        .sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1)[0]
 };
 
 // Do not edit below this line
