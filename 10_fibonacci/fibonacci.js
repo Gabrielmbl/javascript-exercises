4b5335d9b2305ab0b9baacc2c3abdcacbb42de43
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-04-23 15:55:30 -0400

    Finalized 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    n = parseInt(num)
 
+    switch(true) {
+        case (n < 0):
+            return "OOPS";
+        case (n === 0):
+            return 0;
+        case (n === 1):
+            return 1;
+        default:
+            // Handle other cases
+            break;
+    }
+    
+    let prev = 0
+    let current = 1
+
+    for (let i=2; i <= n; i++) {
+        let next = prev + current
+        prev = current
+        current = next
+    }
+
+    return current
 };
 
 // Do not edit below this line
