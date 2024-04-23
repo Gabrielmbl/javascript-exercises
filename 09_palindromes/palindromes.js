abc8b370eb19c8a6bc164a26bf5436565094cb54
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-04-23 15:20:41 -0400

    Finalized 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumericalString = str
+        .toLowerCase()
+        .split("")
+        .filter((character) => (character >= "a" && character <= "z") ||
+            !isNaN(character) && 
+            character != " ")
+        .join("")
+
+    const reversedStr = alphanumericalString.split("").reverse().join("")
+
+    console.log(`alphanumericalString = ${alphanumericalString}`)
+    console.log(`reversedStr = ${reversedStr}`)
+    return alphanumericalString === reversedStr
 
 };
+  
 
 // Do not edit below this line
 module.exports = palindromes;
